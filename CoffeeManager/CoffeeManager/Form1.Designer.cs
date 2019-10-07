d790ae223fe8fc615f6d25a69d5f5b13bfa15d79
Huy Hoang <Huyhoangvole@gmail.com>
2019-10-07 20:07:51 +0700

    no message

--- CoffeeManager/CoffeeManager/Form1.Designer.cs
+++ CoffeeManager/CoffeeManager/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -43,23 +44,33 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(189, 12);
+            this.button2.Location = new System.Drawing.Point(123, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 45);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 22);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1347, 602);
+            this.ClientSize = new System.Drawing.Size(1366,768 );
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "CoffeeManager";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +78,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
 
 
     }
--- CoffeeManager/CoffeeManager/Form1.cs
+++ CoffeeManager/CoffeeManager/Form1.cs
@@ -26,5 +26,10 @@ namespace CoffeeManager
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
